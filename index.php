dda592ba2b2f0b0815ca2e7593857d2807e80119
gabrieldealmeida1001@gmail.com <gabrieldealmeida1001@gmail.com>
2026-05-12 20:07:51 -0300

    [add] fiz o html necessario ate agora

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>TypeGame</title>
+    <link rel="stylesheet" href="style.css" />
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <header>
+      <img class="iconImagem" src="./assets/patoIcon.jpg" alt="" />
+      <h1>TypeGame</h1>
+      <nav class="divNavegacao">
+        <ul>
+          <li>
+            <img
+              class="iconNav"
+              src="./assets/historia.png"
+              alt="historicoIcon"
+            />
+          </li>
+          <li>
+            <img class="iconNav" src="./assets/coroa.png" alt="coroaIcon" />
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <div class="divBotoes">
+      <button class="btnComecar">Começar</button>
+      <button class="btnReniciar">Reniciar</button>
+    </div>
+    <main class="mainGame">
+      <div class="divPalavras">
+        Em, um, bom, e, verdadeiro, não, um, conjunto, escolar, eles, afirmam,
+        que, a, alta, vida, considera, e, não, vem, o, que, também, para, o,
+        ponto, definido, pode, querer, como, enquanto
+      </div>
+    </main>
+    <div class="divPontuacao">
+      <h1>divPontuação:</h1>
+      <h2>100</h2>
+    </div>
+  </body>
+</html>
