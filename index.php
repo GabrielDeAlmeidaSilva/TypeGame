303a57749172234fc1bd3e0527270ef98b18064b
Eduardo Grando Lopes <eduardogl281207@gmail.com>
2026-05-12 21:50:32 -0300

    css malfeito mas preciso dormir

--- index.php
+++ index.php
@@ -9,37 +9,41 @@
   </head>
   <body>
     <header>
-      <img class="iconImagem" src="./assets/patoIcon.jpg" alt="" />
-      <h1>TypeGame</h1>
-      <nav class="divNavegacao">
-        <ul>
-          <li>
-            <img
-              class="iconNav"
-              src="./assets/historia.png"
-              alt="historicoIcon"
-            />
-          </li>
-          <li>
-            <img class="iconNav" src="./assets/coroa.png" alt="coroaIcon" />
-          </li>
-        </ul>
-      </nav>
-    </header>
-    <div class="divBotoes">
-      <button class="btnComecar">Começar</button>
-      <button class="btnReniciar">Reniciar</button>
-    </div>
-    <main class="mainGame">
-      <div class="divPalavras">
-        Em, um, bom, e, verdadeiro, não, um, conjunto, escolar, eles, afirmam,
-        que, a, alta, vida, considera, e, não, vem, o, que, também, para, o,
-        ponto, definido, pode, querer, como, enquanto
+      <div class="cabecalho">
+        <img class="iconImagem" src="./assets/patoIcon.png" alt="" />
+        <h1>TypeGame</h1>
+        <nav class="divNavegacao">
+          <ul>
+            <li>
+              <img
+                class="iconNav"
+                src="./assets/historia.png"
+                alt="historicoIcon"
+              />
+            </li>
+            <li>
+              <img class="iconNav" src="./assets/coroa.png" alt="coroaIcon" />
+            </li>
+          </ul>
+        </nav>
       </div>
-    </main>
-    <div class="divPontuacao">
-      <h1>divPontuação:</h1>
-      <h2>100</h2>
+    </header>
+    <div id="containerGame">
+      <div class="divBotoes">
+        <button class="btnComecar">Começar</button>
+        <button class="btnReniciar">Reniciar</button>
+      </div>
+      <main class="mainGame">
+        <div class="divPalavras">
+          Em, um, bom, e, verdadeiro, não, um, conjunto, escolar, eles, afirmam,
+          que, a, alta, vida, considera, e, não, vem, o, que, também, para, o,
+          ponto, definido, pode, querer, como, enquanto
+        </div>
+      </main>
+      <div class="divPontuacao">
+        <h1>Pontuação:</h1>
+        <h2>100</h2>
+      </div>
     </div>
   </body>
 </html>
